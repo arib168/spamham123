bbfd1a035c2d49fa637ac7d1f7228ba9ca32602f
Arib Nawal <65786174+arib168@users.noreply.github.com>
2022-05-31 16:11:13 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -11,4 +11,4 @@ ip = st.text_input("Enter your message:")
 #predict if the entered message is spam or ham 
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
-  st.title(op[0])  #prints the output as spam or ham 
+  st.title(op[0])  #prints the output as spam or ham  
